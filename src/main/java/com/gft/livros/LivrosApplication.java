763718674fef353fc1c368a1b9d0a824c34fea51
dodn <diego.lourenco@gft.com>
2021-04-20 11:07:28 -0300

    starting project

---
+++ src/main/java/com/gft/livros/LivrosApplication.java
@@ -0,0 +1,13 @@
+package com.gft.livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gft/livros/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gft.livros;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LivrosApplication.class);
+	}
+
+}
