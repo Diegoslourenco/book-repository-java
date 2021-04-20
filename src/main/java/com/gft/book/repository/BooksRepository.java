0ae42bff6a5cf835374c573e35c33cd7c3565738
Diegoslourenco <diego.lourenco15@gmail.com>
2021-04-20 19:10:09 -0300

    add BookRepository class

---
+++ src/main/java/com/gft/book/repository/BooksRepository.java
@@ -0,0 +1,13 @@
+package com.gft.book.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gft.book.model.Book;
+
+public interface BooksRepository extends  JpaRepository<Book, Long> {
+	
+	public List<Book> findByTitleContaining(String title);
+}
+
